a1eb107534f80f5a485291a6e995b14befbfafe1
Jack <mvfd.jackst@gmai.com>
2020-02-26 20:06:42 -0600

    added git instructions

---
+++ git_instructions.py
@@ -0,0 +1,6 @@
+# git clone <url>                       copying a repository (only 1 per repo)
+# git status                            (files you made changes = red) (green = commited files)
+# git add .                             (. does all changes, <filename> for single file)
+# git commit -m "Some message Here"     (prep to push)
+# git push origin master                (push file out)
+# git pull                              (update all repo)
