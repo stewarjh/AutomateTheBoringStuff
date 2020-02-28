3ec7a5b4496b8255d6d7e6825bbbb8efc3820010
Jack <mvfd.jackst@gmai.com>
2020-02-28 04:25:07 -0600

    update to def

--- git_instructions.py
+++ git_instructions.py
@@ -1,4 +1,4 @@
-# git clone <url>                       copying a repository (only 1 per repo)
+git add .# git clone <url>                       copying a repository (only 1 per repo)
 # git status                            (files you made changes = red) (green = commited files)
 # git add .                             (. does all changes, <filename> for single file)
 # git commit -m "Some message Here"     (prep to push)
