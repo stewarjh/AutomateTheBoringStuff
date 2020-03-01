eeceebf214493a9b609e7d01c46316360daa74f3
Jack <mvfd.jackst@gmai.com>
2020-02-29 20:10:04 -0600

    more updates to moving files

--- moving_files_to_a_folder_chris_fixed.py
+++ moving_files_to_a_folder_chris_fixed.py
@@ -1,26 +1,42 @@
 # Move File Types From One Folder to Another
 import os, shutil
 
-
+# Folder that file or files are located.
 def getFromFolder():
-	print('Where do you want to get the file from? EX: C:\\\\Users\\\\')
-	return input()
+	return str(input('Where do you want to get the file from?\n'))
 
 
+# Folder where file or files will be moved.
 def getToFolder():
-	print('Where do you want to put your file? EX: C:\\\\Users\\\\')
-	return input()
+	return str(input('Where do you want to put your file?\n'))
 
 
+# Uses file type EX .py .txt. png to select multiple files.
 def chooseFileType():
-	print('What file type do you want to move? EX: .txt, .py, etc...')
-	return input()
+	return str(input('What file type do you want to move? EX: .txt, .py, etc...\n'))
 
+
+# Uses the filename to select file .
 def fileName():
-	print('What file do you want to move? EX: HelloWorld.txt')
-	return input()
+	return str(input('What file do you want to move? EX: HelloWorld.txt\n'))
 
 
+# Selects folder you want to move.
+def subFolderName():
+    return str(input('Enter Selected folder name:\n'))
+
+
+# Moved folders location.
+def moveFolderTo():
+    return str(input('Enter folder destination:\n'))
+
+
+# Moves a folder to another location
+def moveFolder(selectFolder, moveFolder):
+    shutil.move(selectFolder, moveFolder,)
+
+
+# Moves file or files of the same type to another folder.
 def moveFileTypes(fromFolder, toFolder, fileType):
 	for folderName, subfolders, filenames in os.walk(fromFolder):
 		for file in filenames:
@@ -30,6 +46,7 @@ def moveFileTypes(fromFolder, toFolder, fileType):
 				os.remove(fullyQualifiedFile)
 
 
+# Moves a file from one location to another.
 def moveFileName(fromFolder, toFolder, thisFile):
 	for folderName, subfolders, filenames in os.walk(fromFolder):
 		for file in filenames:
@@ -39,21 +56,24 @@ def moveFileName(fromFolder, toFolder, thisFile):
 				os.remove(fullyQualifiedFile)
 
 
-def app():
+# Commands to move folder
+def appfolderMove():
+    selectFolder = subFolderName()
+    folderMoveTo = moveFolderTo()
+    moveFolder(selectFolder, folderMoveTo)
+
+
+# Used to call moveFileType
+def appMoveFileType():
 	fileType = chooseFileType()
 	fromFolder = getFromFolder()
 	toFolder = getToFolder()
 	moveFileTypes(fromFolder, toFolder, fileType)
 
 
-def app2():
+# Used to call moveFileName
+def appMoveFileName():
 	fromFolder = getFromFolder()
 	toFolder = getToFolder()
 	thisFile = fileName()
 	moveFileName(fromFolder, toFolder, thisFile)
-
-
-#app()
-
-
-app2()
--- moving_files_to_a_folder_mac.py
+++ moving_files_to_a_folder_mac.py
@@ -1,22 +1,42 @@
 # Move File Types From One Folder to Another
 import os, shutil
 
-
+# Folder that file or files are located.
 def getFromFolder():
-	print('Where do you want to get the file from?')
-	return input()
+	return str(input('Where do you want to get the file from?\n'))
 
 
+# Folder where file or files will be moved.
 def getToFolder():
-	print('Where do you want to put your file?')
-	return input()
+	return str(input('Where do you want to put your file?\n'))
 
 
+# Uses file type EX .py .txt. png to select multiple files.
 def chooseFileType():
-	print('What file type do you want to move? EX: .txt, .py, .png, etc...')
-	return input()
+	return str(input('What file type do you want to move? EX: .txt, .py, etc...\n'))
 
 
+# Uses the filename to select file .
+def fileName():
+	return str(input('What file do you want to move? EX: HelloWorld.txt\n'))
+
+
+# Selects folder you want to move.
+def subFolderName():
+    return str(input('Enter Selected folder name:\n'))
+
+
+# Moved folders location.
+def moveFolderTo():
+    return str(input('Enter folder destination:\n'))
+
+
+# Moves a folder to another location
+def moveFolder(selectFolder, moveFolder):
+    shutil.move(selectFolder, moveFolder,)
+
+
+# Moves file or files of the same type to another folder.
 def moveFileTypes(fromFolder, toFolder, fileType):
 	for folderName, subfolders, filenames in os.walk(fromFolder):
 		for file in filenames:
@@ -26,11 +46,34 @@ def moveFileTypes(fromFolder, toFolder, fileType):
 				os.remove(fullyQualifiedFile)
 
 
-def app():
+# Moves a file from one location to another.
+def moveFileName(fromFolder, toFolder, thisFile):
+	for folderName, subfolders, filenames in os.walk(fromFolder):
+		for file in filenames:
+			if file == thisFile: # can move file types by [if file.endswith('.py'):]
+				fullyQualifiedFile = fromFolder + os.sep + file
+				shutil.copy(fullyQualifiedFile, toFolder)
+				os.remove(fullyQualifiedFile)
+
+
+# Commands to move folder
+def appfolderMove():
+    selectFolder = subFolderName()
+    folderMoveTo = moveFolderTo()
+    moveFolder(selectFolder, folderMoveTo)
+
+
+# Used to call moveFileType
+def appMoveFileType():
 	fileType = chooseFileType()
 	fromFolder = getFromFolder()
 	toFolder = getToFolder()
 	moveFileTypes(fromFolder, toFolder, fileType)
 
 
-app()
+# Used to call moveFileName
+def appMoveFileName():
+	fromFolder = getFromFolder()
+	toFolder = getToFolder()
+	thisFile = fileName()
+	moveFileName(fromFolder, toFolder, thisFile)
